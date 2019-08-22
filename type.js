b6140c53ca1a75b5b1f8ad8fdb4d8ab725874bf5
neto soto <Ernesto.Soto.1998@hotmail.com>
2019-08-21 22:12:41 -0600

    commit Interfaces

--- type.js
+++
@@ -1,5 +0,0 @@
-function area(shape, width, height) {
-    var area = width * height;
-    return "Yo soy " + shape + " con area de " + area + " cm²";
-}
-document.body.innerHTML = area("rectangulo", 30, 15);
--- type.ts
+++
@@ -1,6 +0,0 @@
-function area(shape: string, width: number, height: number){
-    var area = width * height;
-    return "Yo soy " + shape + " con area de " + area + " cm²";
-}
-
-document.body.innerHTML = area("rectangulo", 30, 15);
