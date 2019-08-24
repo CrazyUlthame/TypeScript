02a4fefaaf074c0b08b76a5bf9cb0a98b38a6668
neto soto <Ernesto.Soto.1998@hotmail.com>
2019-08-23 20:21:03 -0600

    commit Interfaces

--- multiplos.ts
+++
@@ -1,8 +0,0 @@
-let a: number = 12;
-let b: number = 17;
-
-function showProduct(first: number, second: number) : void{
-    console.log("the product is: " + first * second);    
-}
-
-showProduct(a,b);
