35fa41a84b8ff0d6f1fcafc7851c10651cb8ab66
neto soto <Ernesto.Soto.1998@hotmail.com>
2019-08-24 19:10:12 -0600

    commit Genericos

---
+++ genericos.ts
@@ -0,0 +1,10 @@
+function randomElem<T>(theArray: T[]): T{
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}
+
+
+let colors: string[] = ['violet','indigo','blue','green'];
+let randomColor: string = randomElem(colors);
+
+console.log(randomColor);
